b049d741c9ffa6c350ca0fe2d25532146cb7a59b
kmb5 <20370225+kmb5@users.noreply.github.com>
2020-12-15 15:30:20 +0100

    solved part2

--- day15.py
+++ day15.py
@@ -4,16 +4,26 @@ def main():
 
     numbers = [12,20,0,6, 1, 17, 7]
 
+    numbers_seen = {n: [i+1] for i, n in enumerate(numbers[:-1])}
+
     for i in tqdm(range(7, 30000000)):
 
-        prev_num = numbers[i - 1]
+        #print(f'TURN {i}')
 
-        if prev_num not in numbers[:-1]:
+        prev_num = numbers[i - 1] # 6
+
+        if prev_num not in numbers_seen.keys():
             numbers.append(0)
+            numbers_seen[prev_num] = [i]
         else:
-            indices = [n for n, x in enumerate(numbers) if x == prev_num]
-            num_to_append = (indices[-1] + 1) - (indices[-2] + 1)
+            numbers_seen[prev_num].append(i)
+            #print(numbers_seen)
+            indices = numbers_seen[prev_num]
+            num_to_append = (indices[-1]) - (indices[-2])
             numbers.append(num_to_append)
+        
+        #print(numbers)
+        #print()
 
     
     print(numbers[-1])

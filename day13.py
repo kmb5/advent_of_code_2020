9f971651cadb883c9ca991b97854435a029d84a2
kmb5 <20370225+kmb5@users.noreply.github.com>
2021-01-05 18:24:31 +0100

    solved day13

--- day13.py
+++ day13.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from helpers import readlines
 
 def main():
@@ -5,6 +6,7 @@ def main():
     data = readlines('inputs/day13_input.txt')
 
     print(part1(data))
+    print(part2(data))
 
 
 def part1(data):
@@ -20,10 +22,26 @@ def part1(data):
                 return (current_timestamp - original_timestamp) * bus
         
         current_timestamp += 1
-        
 
+def part2(data):
 
+    '''Prepare chinese remainder such as:
+    In the given example: 7,13,x,x,59,x,31,19 we're basically trying to find a time t where:
+    t % 7 == 0 
+    t % 13 == 12 (the remainder we're looking for is 12 because we want bus 13 to depart at t+1, so essentially we want (t+1) % 13 == 0) 
+    t % 59 == 55 (we want bus 59 to depart at t+4) 
+    ...
+    ....
+    etc.
+    '''
 
+    bus_numbers = [int(el) if el != 'x' else el for el in data[1].split(',')]
+    timestamps = [i for i in range(len(bus_numbers)) if bus_numbers[i] != 'x']
+    timestamps = [i if i == 0 else bus_numbers[i] - i for i in timestamps]
+
+    bus_numbers = [i for i in bus_numbers if i != 'x']
+
+    return chinese_remainder(bus_numbers, timestamps)
 
 def visualise():
 
@@ -47,6 +65,24 @@ def visualise():
         print(str(second) + '\t' + ('\t').join(departures))
 
 
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a*b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
+
 if __name__ == "__main__":
     main()
-
--- day14.py
+++ day14.py
@@ -1,4 +1,5 @@
 from re import search
+from itertools import product
 from helpers import readlines
 
 def main():
@@ -6,6 +7,7 @@ def main():
     data = readlines('inputs/day14_input.txt')
     
     print(part1(data))
+    print(part2(data))
 
 def part1(data):
 
@@ -24,19 +26,54 @@ def part1(data):
             address = address.group(0)
 
             value = int(split[1])
-
-
             masked_value = put_mask(int2bin(value), mask)
-
             memory[address] = int(masked_value, 2)
+
+    return sum([x for x in memory.values() if x != 0])
+
+def part2(data):
+
+    memory = {}
+    mask = 'X' * 36
+
+    for line in data:
+        split = line.split(' = ')
+        if split[0] == 'mask':
+            mask = split[1]
+        else:
+            address = search(r'(?<=\[)[0-9]+(?=\])', split[0])
+            address = address.group(0)
+
+            value = int(split[1])
+            address_bin = int2bin(int(address))
+
+            #print(f'address:\t{address_bin}')
+            #print(f'mask:\t\t{mask}')
+            masked_value = put_mask_pt2(address_bin, mask)
+            #print(f'result:\t\t{masked_value}')
+
+            #print('\n')
+            decoded = decode_addresses(masked_value)
+            
+            for address in decoded:
+                memory[address] = value
 
     return sum([x for x in memory.values() if x != 0])
 
 
 
-    
-    print(mask)
+def decode_addresses(masked_value):
 
+    all_decoded_addresses = list(_filler(masked_value, 'X'))
+
+    all_decoded_addresses_int = [int(x, 2) for x in all_decoded_addresses]
+    
+    return all_decoded_addresses_int
+
+
+def _filler(word, from_char):
+    options = [(c,) if c != from_char else ('0','1') for c in word]
+    return (''.join(o) for o in product(*options))
 
 def put_mask(value, mask):
 
@@ -47,6 +84,19 @@ def put_mask(value, mask):
         if n != 'X':
             new_value[i] = n
 
+    return ('').join(new_value)
+
+def put_mask_pt2(value, mask):
+
+    new_value = [char for char in value]
+
+    for i, n in enumerate(mask):
+
+        if n == '1':
+            new_value[i] = '1'
+        elif n == 'X':
+            new_value[i] = 'X'
+    
     return ('').join(new_value)
 
 
